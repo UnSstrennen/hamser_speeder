bbf0ab2772b8fb3555e728c0282fb2e70dfa0639
Un Sstrennen <n-i-k-i-t@yandex.ru>
2019-09-01 11:13:30 +0300

    continue developing

--- src/main.c
+++ src/main.c
@@ -1,12 +1,20 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
+#define M_PI 3.14159265358979323846
+
 #define button PB1
 #define hall PB3
+#define pot PB4
 
 ISR(INT0_vect)
 {
   // change the mode
+}
+
+ISR(ANALOG_COMP_vect)
+{
+  // magnet detected
 }
 
 int main (void)
@@ -17,10 +25,15 @@ int main (void)
   // EXINT
   GIMSK |= (1<<INT0);
   MCUCR |= (1<<ISC00)|(1<<ISC01);
+
+  // comparator
+  ACSR |= (1<<ACIS0)|(1<<ACIS1);  // choose the mode
+  ACSR |= (1<<ACIE);  // allow interrupts
+
   sei();
 
   while(1)
   {
-    if ()
+    
   }
 }
